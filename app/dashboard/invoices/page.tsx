3180450d9fa3ee9199e6c964d482b4820a937790
suttikhun sihsobhon <suttikhun.sihsobhon@lseg.com>
2024-06-30 16:22:27 +0700

    setup next tutorial upto ch6

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <p>invoices page</p>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <p>dashboard page</p>
+  )
+}
+
+export default Page
